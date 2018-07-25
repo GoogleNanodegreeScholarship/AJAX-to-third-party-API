14b7b98b3588412111e665c8b101c07f4ff17bcc
americanman <snowboardracer7@gmail.com>
2018-07-25 15:51:32 -0400

    Make AJAX request to unsplash.com with jQuery method

--- lesson-2-async-w-jQuery/app.js
+++ lesson-2-async-w-jQuery/app.js
@@ -1,14 +1,32 @@
 /* eslint-env jquery */
 
 (function () {
-    const form = document.querySelector('#search-form');
-    const searchField = document.querySelector('#search-keyword');
-    let searchedForText;
-    const responseContainer = document.querySelector('#response-container');
+  const form = document.querySelector('#search-form');
+  const searchField = document.querySelector('#search-keyword');
+  let searchedForText;
+  const responseContainer = document.querySelector('#response-container');
 
-    form.addEventListener('submit', function (e) {
-        e.preventDefault();
-        responseContainer.innerHTML = '';
-        searchedForText = searchField.value;
-    });
+  form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    responseContainer.innerHTML = '';
+    searchedForText = searchField.value;
+
+    $.ajax({
+    url: `https://api.unsplash.com/search/photos?page=1&query=${searchedForText}`,
+    headers: {
+      Authorization: 'Client-ID ed4ea3b388f4503fa9a5817e2e5250171fd92b3b61ff520ff9f6027cff251a67'
+    }
+  }).done(addImage);
+  });
+
+  function addImage(images) {
+    const firstImage = images.results[0];
+
+    responseContainer.insertAdjacentHTML('afterbegin', `<figure>
+            <img src="${firstImage.urls.small}" alt="${searchedForText}">
+            <figcaption>${searchedForText} by ${firstImage.user.name}</figcaption>
+        </figure>`
+    );
+}
+
 })();
