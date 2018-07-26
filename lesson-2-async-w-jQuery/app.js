d19772f400d0196133f385895bf71cebe422a733
americanman <snowboardracer7@gmail.com>
2018-07-26 11:04:25 -0400

    Make AJAX request to NY Times with jQuery method, map through response array

--- lesson-2-async-w-jQuery/app.js
+++ lesson-2-async-w-jQuery/app.js
@@ -12,21 +12,34 @@
     searchedForText = searchField.value;
 
     $.ajax({
-    url: `https://api.unsplash.com/search/photos?page=1&query=${searchedForText}`,
-    headers: {
-      Authorization: 'Client-ID ed4ea3b388f4503fa9a5817e2e5250171fd92b3b61ff520ff9f6027cff251a67'
-    }
-  }).done(addImage);
+      url: `https://api.unsplash.com/search/photos?page=1&query=${searchedForText}`,
+      headers: {
+        Authorization: 'Client-ID ed4ea3b388f4503fa9a5817e2e5250171fd92b3b61ff520ff9f6027cff251a67'
+      }
+    }).done(addImage);
+
+    $.ajax({
+      url: `http://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForText}&api-key=0fd16f9ca25b4e0ea140b5e5aa1aa085`
+    }).done(addArticle);
   });
+
+  function addArticle(article) {
+    let htmlContent = '';
+    const articles = article.response.docs;
+		let map = articles.map(article =>
+      htmlContent = '<ul>' + responseContainer.insertAdjacentHTML('afterbegin', `<li class="article"><h2><a href="${article.web_url}">
+      ${article.snippet}</a></h2></li>` + '</ul>'
+		));
+  }
 
   function addImage(images) {
     const firstImage = images.results[0];
 
     responseContainer.insertAdjacentHTML('afterbegin', `<figure>
-            <img src="${firstImage.urls.small}" alt="${searchedForText}">
+            <img src="${firstImage.urls.full}" alt="${searchedForText}">
             <figcaption>${searchedForText} by ${firstImage.user.name}</figcaption>
         </figure>`
     );
-}
+  }
 
 })();
